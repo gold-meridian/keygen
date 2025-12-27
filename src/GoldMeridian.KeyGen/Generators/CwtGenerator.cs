c4111f6b2ae28c2677f60ce12bc6a3aaf4b87601
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2025-12-26 22:17:00 -0600

    feat: init attribute generation

---
+++ src/GoldMeridian.KeyGen/Generators/CwtGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using GoldMeridian.KeyGen.Utilities;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace GoldMeridian.KeyGen;
+
+public sealed class CwtGenerator : IIncrementalGenerator
+{
+    void IIncrementalGenerator.Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        context.RegisterPostInitializationOutput(
+            ctx =>
+            {
+                ctx.PolyfillAddEmbeddedAttributeDefinition();
+                ctx.AddSource(
+                    "ExtensionDataForAttribute.g.cs",
+                    SourceText.From(
+                        """
+                        using System;
+                        using Microsoft.CodeAnalysis;
+                        
+                        namespace GoldMeridian.CodeAnalysis;
+                        
+                        [Embedded]
+                        [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+                        internal sealed class ExtensionDataForAttribute(string? name = null) : Attribute
+                        {
+                            public string? Name => name;
+                        }
+                        """,
+                        Encoding.UTF8
+                    )
+                );
+            }
+        );
+    }
+}
