d0dc062298f2208c6c52fa7a6d87bfa3ed6c3c31
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2025-12-26 22:22:27 -0600

    feat: scaffold quick example to work with and fix generator not being registered

---
+++ src/GoldMeridian.KeyGen.TestProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using GoldMeridian.CodeAnalysis;
+
+namespace GoldMeridian.KeyGen.TestProject;
+
+public class MyData
+{
+    public int A { get; set; }
+}
+
+[ExtensionDataFor<MyData>]
+public class MyDataExtension
+{
+    public string? B { get; set; }
+}
+
+internal static class Program
+{
+    public static void Main()
+    {
+        
+    }
+}
--- src/GoldMeridian.KeyGen/Generators/CwtGenerator.cs
+++ src/GoldMeridian.KeyGen/Generators/CwtGenerator.cs
@@ -5,6 +5,7 @@ using Microsoft.CodeAnalysis.Text;
 
 namespace GoldMeridian.KeyGen;
 
+[Generator]
 public sealed class CwtGenerator : IIncrementalGenerator
 {
     void IIncrementalGenerator.Initialize(IncrementalGeneratorInitializationContext context)
@@ -17,6 +18,8 @@ public sealed class CwtGenerator : IIncrementalGenerator
                     "ExtensionDataForAttribute.g.cs",
                     SourceText.From(
                         """
+                        #nullable enable
+                        
                         using System;
                         using Microsoft.CodeAnalysis;
                         
@@ -24,7 +27,8 @@ public sealed class CwtGenerator : IIncrementalGenerator
                         
                         [Embedded]
                         [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
-                        internal sealed class ExtensionDataForAttribute(string? name = null) : Attribute
+                        internal sealed class ExtensionDataForAttribute<TKey>(string? name = null) : Attribute
+                            where TKey : class
                         {
                             public string? Name => name;
                         }
